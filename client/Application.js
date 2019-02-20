00b78daef2f97292ecf34c455f4cbc284d603eee
Tim Bogatchev <tbogatchev@gmail.com>
2019-02-19 22:43:40 -0500

    get timeseries working.

--- client/Application.js
+++ client/Application.js
@@ -6,21 +6,45 @@ export default class Application extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      averages: []
+      cpuData: [
+          {time: 0, val: .5},
+          {time: 10, val: .55},
+          {time: 20, val: .65},
+          {time: 30, val: .75},
+          {time: 40, val: 1.5},
+          {time: 50, val: .5},
+          {time: 60, val: .55},
+          {time: 70, val: .75},
+          {time: 80, val: .5},
+      ],
+      time: 80
     };
   }
 
   componentDidMount() {
-    // socket = new WebSocket('wss://ws.blockchain.info/inv');
-
-    // socket.onopen = function () {
-    //   socket.send(JSON.stringify({ op: "unconfirmed_sub"}));
-    // };
-    // socket.onmessage = this.handleSocketMessage.bind(this);
+    this.interval = setInterval(this.fetchCPUData.bind(this), 10000);
   }
 
   componentWillUnmount() {
-    //socket.close();
+    clearInterval(this.interval);
+  }
+
+  fetchCPUData() {
+    return fetch('/loadTimes')
+    .then(response => response.json())
+    .then(response => {
+      const newTime = this.state.time + 10;
+      const newDataPoint = [{
+        time: newTime,
+        val: response.average
+      }];
+      //We do this to create a new object reference
+      const newCPUData = this.state.cpuData.concat(newDataPoint);
+      this.setState({
+        cpuData: newCPUData,
+        time: newTime
+      });
+    })
   }
 
   render() {
@@ -28,7 +52,7 @@ export default class Application extends React.Component {
       <div>
         <div className="title">CPU Performance</div>
         <div className="streamContainer container">
-          <Timeseries></Timeseries>
+          <Timeseries data={this.state.cpuData}></Timeseries>
         </div>
       </div>
     );
